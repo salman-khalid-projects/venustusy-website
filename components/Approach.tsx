7fb03405eeeebc91afd4e763c32fa36520aebdfe
ak <ak.personalprojects@gmail.com>
2024-06-18 11:12:45 +0300

    Updated our approach

--- components/Approach.tsx
+++ components/Approach.tsx
@@ -15,7 +15,7 @@ const Approach = () => {
         <Card
           title="Planning & Strategy"
           icon={<AceternityIcon order="Phase 1" />}
-          des="We'll collaborate to map out your website's goals, target audience, 
+          des="We will collaborate to map out your solution's goals, target audience, 
           and key functionalities. We'll discuss things like site structure, 
           navigation, and content requirements."
         >
