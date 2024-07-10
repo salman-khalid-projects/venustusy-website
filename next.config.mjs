6c68f55513fe2c002b697fc979dd80dbd8f60747
Salman <sk.personalprojects@gmail.com>
2024-07-10 21:12:51 +0300

    Modify projects section with the latest updates about the recent projects

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 import { withSentryConfig } from "@sentry/nextjs";
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    swcMinify: false, // Disable SWC minification to use the JavaScript fallback
+};
 
 export default withSentryConfig(withSentryConfig(nextConfig, {
 // For all available options, see:
